ea33beec22aea93a5196d924d8221ae06b6a428a
Nickolay Semutkin <nicksemu42@gmail.com>
2023-11-21 09:41:01 +0300

    changed api.ts, commented pte-commit

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -3,9 +3,6 @@ import { USER_LOGIN_DATA } from '../consts/user';
 
 export const $api = axios.create({
     baseURL: __API__,
-    headers: {
-        authorization: JSON.stringify(localStorage.getItem(USER_LOGIN_DATA)),
-    },
 });
 
 $api?.interceptors.request.use((config) => {
